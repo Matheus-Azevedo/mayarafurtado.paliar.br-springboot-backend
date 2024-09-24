709e84f0caa59d8f9b5333a4bd0aff80f5121b99
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2024-09-24 09:03:35 -0300

    updated testimony annotations

--- src/main/java/br/paliar/mayarafurtado/backend/domain/testimony/TestimonyModel.java
+++ src/main/java/br/paliar/mayarafurtado/backend/domain/testimony/TestimonyModel.java
@@ -8,11 +8,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "testimonials")
 @Entity(name = "Testimony")
+@EqualsAndHashCode(of = "id")
 public class TestimonyModel implements Serializable {
 
   @Id
